68b1919f397b4e3f107bb664c8d3fead8ba388f9
kmchale1974 <61988872+kmchale1974@users.noreply.github.com>
2025-08-06 14:28:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Romeoville Events script is running...");
 
-const FEED_URL = 'https://your-proxy-url/https://www.romeoville.org/RSSFeed.aspx?ModID=58&CID=All-calendar.xml';
+const FEED_URL = 'https://corsproxy.io/?https://www.romeoville.org/RSSFeed.aspx?ModID=58&CID=All-calendar.xml';
 const EVENTS_PER_PAGE = 10;
 let currentPage = 0;
 let pages = [];
